6a09bbf095d14743ddee9f52dd73b43da9b9dd60
JayBal <jackybaloyi.jb@gmail.com>
2022-06-21 19:53:20 -0700

    Betty style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
-/*
- * _memset - fill n bytes of memory with a constant value
- * @memset: - fill
- * @s: pointer to memory area
- * @b: constant value
- * @n: number of bytes to fill
- * Return: memory area
- */
+/**
+  *_memset - fills memory with a constant byte.
+  *fills the first n bytes of the memory area pointed
+  *to by s with the constant b.
+  *@s: pointer to memory area.
+  *@b: constant to fill memory with.
+  *@n: bytes of the memory area to be filled.
+  *
+  *Return: pointer to memory area s.
+  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n) /* fills memory */
 {
 
 	int size = n; /* only accept positive sizes */
